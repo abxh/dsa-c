e5e8f901e64cbce4a9fdd8f74a94d62e0c98f888
abxh <83485102+abxh@users.noreply.github.com>
2025-01-28 21:02:01 +0100

    typo

--- arena/arena_template.h
+++ arena/arena_template.h
@@ -1,6 +1,6 @@
 /*  arena_template.h
  *
- *  Copyright (C) 202f abxh
+ *  Copyright (C) 2025 abxh
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
